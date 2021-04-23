3d440d81a4d93c58ef06f585510b13f43a457f2f
Lyamir <xlatiax@gmail.com>
2021-04-23 23:44:16 +0800

    Update MainActivity.kt

--- app/src/main/java/com/mobdeve/dobleteope/moodegy/MainActivity.kt
+++ app/src/main/java/com/mobdeve/dobleteope/moodegy/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.mobdeve.dobleteope.moodegy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        main_constraint.setOnClickListener {
+            val intent = Intent(this@MainActivity, SettingsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
