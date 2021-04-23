16ac7e89e503d1a655f1fe3662f2b3244770dd68
Lyamir <xlatiax@gmail.com>
2021-04-23 23:06:09 +0800

    Added activities

---
+++ app/src/main/java/com/mobdeve/dobleteope/moodegy/MoodEntryActivity.kt
@@ -0,0 +1,4 @@
+package com.mobdeve.dobleteope.moodegy
+
+class MoodEntryActivity {
+}
---
+++ app/src/main/java/com/mobdeve/dobleteope/moodegy/UpdateNotificationActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.dobleteope.moodegy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UpdateNotificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_updatenotification)
+    }
+}
