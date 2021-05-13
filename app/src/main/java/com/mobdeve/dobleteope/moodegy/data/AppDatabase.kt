887f244ea95f3b0431ab8f3072373a549aa942e3
Lyamir <xlatiax@gmail.com>
2021-05-14 00:13:46 +0800

    Added DB Checker

--- app/src/main/java/com/mobdeve/dobleteope/moodegy/data/AppDatabase.kt
+++ app/src/main/java/com/mobdeve/dobleteope/moodegy/data/AppDatabase.kt
@@ -1,7 +1,10 @@
 package com.mobdeve.dobleteope.moodegy.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(
     entities = [
@@ -15,6 +18,29 @@ import androidx.room.RoomDatabase
     version = 1,
     exportSchema = false
 )
+@TypeConverters (Converters::class)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun moodEntryDao(): MoodEntryDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase{
+            val tempInstance = INSTANCE
+            if(tempInstance != null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "MoodegyDB"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+
 }
