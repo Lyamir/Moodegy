f6f38c18d6def36a57608b879d4c69ff416ed2b1
Lyamir <xlatiax@gmail.com>
2021-05-17 01:47:47 +0800

    Added PrimaryKey AutoGenerate

--- app/src/main/java/com/mobdeve/dobleteope/moodegy/data/Mood.kt
+++ app/src/main/java/com/mobdeve/dobleteope/moodegy/data/Mood.kt
@@ -7,15 +7,6 @@ import androidx.room.Relation
 
 @Entity(tableName = "mood")
 data class Mood(
-    @PrimaryKey val id: Int,
+    @PrimaryKey(autoGenerate = true) val id: Int,
     val name: String
-)
-
-data class AllMoodEntriesWithMood(
-    @Embedded val mood: Mood,
-    @Relation(
-        parentColumn = "id",
-        entityColumn = "moodID"
-    )
-    val entries: List<MoodEntry>
 )
