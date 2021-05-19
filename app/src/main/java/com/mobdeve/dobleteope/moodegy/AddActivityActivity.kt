ea042af3572712d8ef20b1ab6666f37ca38bbcd3
Lyamir <xlatiax@gmail.com>
2021-05-19 21:37:59 +0800

    Added Functionality

--- app/src/main/java/com/mobdeve/dobleteope/moodegy/AddActivityActivity.kt
+++ app/src/main/java/com/mobdeve/dobleteope/moodegy/AddActivityActivity.kt
@@ -1,11 +1,43 @@
 package com.mobdeve.dobleteope.moodegy
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
+import com.mobdeve.dobleteope.moodegy.data.Activity
+import com.mobdeve.dobleteope.moodegy.data.AppDatabase
+import kotlinx.android.synthetic.main.activity_addactivity.*
 
 class AddActivityActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_addactivity)
+
+        addactivity_btn.isEnabled = false
+
+        addactivity_edittext.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                addactivity_btn.isEnabled = s.toString().trim { it <= ' ' }.isNotEmpty()
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int, count: Int,
+                after: Int
+            ) {
+                // TODO Auto-generated method stub
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                // TODO Auto-generated method stub
+            }
+        })
+
+        val db = AppDatabase.getDatabase(this)
+        val activityDao = db.activityDao()
+
+        addactivity_btn.setOnClickListener(){
+            activityDao.insert(Activity(0, addactivity_edittext.text.toString()))
+            finish()
+        }
     }
 }
--- app/src/main/java/com/mobdeve/dobleteope/moodegy/AddMoodActivity.kt
+++ app/src/main/java/com/mobdeve/dobleteope/moodegy/AddMoodActivity.kt
@@ -1,11 +1,43 @@
 package com.mobdeve.dobleteope.moodegy
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
+import com.mobdeve.dobleteope.moodegy.data.AppDatabase
+import com.mobdeve.dobleteope.moodegy.data.Mood
+import kotlinx.android.synthetic.main.activity_addmood.*
 
 class AddMoodActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_addmood)
+
+        addmood_btn.isEnabled = false
+
+        addmood_edittext.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                addmood_btn.isEnabled = s.toString().trim { it <= ' ' }.isNotEmpty()
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int, count: Int,
+                after: Int
+            ) {
+                // TODO Auto-generated method stub
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                // TODO Auto-generated method stub
+            }
+        })
+
+        val db = AppDatabase.getDatabase(this)
+        val moodDao = db.moodDao()
+
+        addmood_btn.setOnClickListener(){
+            moodDao.insert(Mood(0, addmood_edittext.text.toString()))
+            finish()
+        }
     }
 }
