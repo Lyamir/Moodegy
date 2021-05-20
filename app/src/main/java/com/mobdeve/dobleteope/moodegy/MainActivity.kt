551f6e8d9f3b3a1f65d21506639213d30bc2d638
Lyamir <xlatiax@gmail.com>
2021-05-20 11:17:17 +0800

    Added Bottom NavBar

--- app/src/main/java/com/mobdeve/dobleteope/moodegy/MainActivity.kt
+++ app/src/main/java/com/mobdeve/dobleteope/moodegy/MainActivity.kt
@@ -1,62 +1,44 @@
 package com.mobdeve.dobleteope.moodegy
 
-import android.content.DialogInterface
-import android.content.Intent
-import android.os.Bundle
-import android.view.Gravity
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.mobdeve.dobleteope.moodegy.data.*
-import kotlinx.android.synthetic.main.activity_main.*
-
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.mobdeve.dobleteope.moodegy.fragments.HomeFragment
+import com.mobdeve.dobleteope.moodegy.fragments.SettingsFragment
+import com.mobdeve.dobleteope.moodegy.fragments.StatsFragment
+import kotlinx.android.synthetic.main.activity_main2.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_stats.setOnClickListener {
-            val intent = Intent(this, StatsActivity::class.java)
-            startActivity(intent)
+        val homeFragment = HomeFragment()
+        val statsFragment = StatsFragment()
+        val settingsFragment = SettingsFragment()
+
+        makeCurrentFragment(homeFragment)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.ic_home -> makeCurrentFragment(homeFragment)
+                R.id.ic_stats -> makeCurrentFragment(statsFragment)
+                R.id.ic_settings -> makeCurrentFragment(settingsFragment)
+            }
+            true
         }
 
-        btn_addmood.setOnClickListener {
-            val intent = Intent(this, AddMoodActivity::class.java)
-            startActivity(intent)
-        }
+    }
 
-        btn_addactivity.setOnClickListener {
-            val intent = Intent(this, AddActivityActivity::class.java)
-            startActivity(intent)
-        }
-
-        btn_addmoodentry.setOnClickListener{
-            val intent = Intent(this, AddMoodEntryActivity::class.java)
-            startActivity(intent)
+    private fun makeCurrentFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper,fragment)
+            commit()
         }
     }
 
     override fun onResume() {
         super.onResume()
-
-        val db = AppDatabase.getDatabase(this)
-        val moodEntryDao = db.moodEntryDao()
-        val moodDao = db.moodDao()
-        val activityDao = db.activityDao()
-        val activityEntryDao = db.activityEntryDao()
-
-        val moodList = moodDao.getAll()
-        val moodEntryList = moodEntryDao.getAll()
-        val activityList = activityDao.getAll()
-        val activityEntryList = activityEntryDao.getAll()
-
-        val adapter = MoodEntryAdapter(moodEntryList, moodList, activityList, activityEntryList)
-        main_recyclerview.adapter = adapter
-        val llm = LinearLayoutManager(this)
-        llm.reverseLayout = true
-        llm.stackFromEnd = true
-        main_recyclerview.layoutManager = llm
-        main_recyclerview.setHasFixedSize(true)
     }
 }
