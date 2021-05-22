587323859f6f654016ef8375d2cafcd8622833e4
teope <46042539+teope@users.noreply.github.com>
2021-05-22 23:45:50 +0800

    Added Add Activity
    Included add activity main, add activity item, activity item

---
+++ app/src/main/java/com/mobdeve/dobleteope/moodegy/AddActivityItemActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.dobleteope.moodegy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddActivityItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.addactivity_item)
+    }
+}
