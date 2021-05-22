857e63de017f70480243ce0c5305597b7a306407
teope <46042539+teope@users.noreply.github.com>
2021-05-23 01:40:07 +0800

    Updated Mood List
    Added update mood, add mood list, mood list

---
+++ app/src/main/java/com/mobdeve/dobleteope/moodegy/AddMoodItemActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.dobleteope.moodegy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddMoodItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.addmood_item)
+    }
+}
