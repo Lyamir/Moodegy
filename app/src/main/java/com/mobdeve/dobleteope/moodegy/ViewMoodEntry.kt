638d3e677f0f796460560351d96bef46ddd6f622
teope <46042539+teope@users.noreply.github.com>
2021-05-22 20:08:28 +0800

    Updated View Entry

---
+++ app/src/main/java/com/mobdeve/dobleteope/moodegy/ViewMoodEntry.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.dobleteope.moodegy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewMoodEntry : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_viewmoodentry)
+    }
+}
