729015c96794e7f3f277eb3a062ad81a49c6ab0e
Lyamir <xlatiax@gmail.com>
2021-05-25 22:49:46 +0800

    Added Lottie

--- app/src/main/java/com/mobdeve/dobleteope/moodegy/IntroductoryActivity.kt
+++ app/src/main/java/com/mobdeve/dobleteope/moodegy/IntroductoryActivity.kt
@@ -3,17 +3,14 @@ package com.mobdeve.dobleteope.moodegy
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.airbnb.lottie.LottieAnimationView
+import kotlinx.android.synthetic.main.activity_introductory.*
 
 class IntroductoryActivity : AppCompatActivity() {
 
-    private lateinit var lottie: LottieAnimationView;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_introductory)
-
-        lottie = findViewById(R.id.lottie);
 
         lottie.animate().setDuration(2000).alpha(1f).withEndAction{
             val i = Intent(this, MainActivity::class.java)
